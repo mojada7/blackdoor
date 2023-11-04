d84a32b0bdfb07e649fde1311163dd78ad150faa
mohammad <M@>
2023-11-04 19:10:00 +0330

    slogan

--- src/components/slogan/index.tsx
+++ src/components/slogan/index.tsx
@@ -3,9 +3,18 @@ import React from 'react'
 function Slogan() {
   return (
     <div className=' w-[60%] text-center overflow-clip'>
-        <div>مرجع آگهی ایران</div>
-        <div>BlackDoor پروژه ایرانی تجربه زندگی متاورسی</div>
-        <div></div>
+        <div className='mb-2'>مرجع آگهی ایران</div>
+        <div className='mb-2 text-sm'>
+            مثل آب خوردن تو متاورس چیز میزاتو بفروش و واسه بیزینست تبلیغ کن
+        </div>
+        <div className='text-sm'>
+            ما از شما برای آگهی گذاشتن پول میگیریم؟
+        </div>
+        <div className='text-sm mb-4'>
+            شما با گذاشتن آگهی هاتون به بزرگ شدن حرفه ما کمک میکنید. نه تنها پول نمیگیریم بلکه تلاش میکنیم شما رو تو این پیشرفت سهیم کنیم
+        </div>
+        <div className='mb-2 text-xs'>BlackDoor پروژه ایرانی تجربه زندگی متاورسی</div>
+        <div className='mb-2'></div>
     </div>
   )
 }
