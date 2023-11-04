f70670a03e30029f4735702029770031e2c34d7a
mohammad <M@>
2023-11-04 18:23:05 +0330

    blackdoornamechooseforfun

--- src/components/firstPageLogo/index.tsx
+++ src/components/firstPageLogo/index.tsx
@@ -5,10 +5,10 @@ import CenterFlexCol from '../centerFlexCol'
 function Logo() {
   return (
     <CenterFlexCol>
-      <div className='h-16 w-16 bg-black mt-10'>
+      <div className='h-24 w-16 bg-black mt-10'>
       </div>
       <div className='mt-1 mb-10'>
-        چند چند
+        BlackDoor
       </div>
     </CenterFlexCol>
   )
--- src/components/slogan/index.tsx
+++ src/components/slogan/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Slogan() {
   return (
-    <div>Slogan</div>
+    <div className=' w-[60%] text-center overflow-clip'>
+        <div>مرجع آگهی ایران</div>
+        <div>BlackDoor پروژه ایرانی تجربه زندگی متاورسی</div>
+        <div></div>
+    </div>
   )
 }
 
