2f697fcf305d5b0182cd0e1631f2362939cf0a1f
mohammad <M@>
2023-11-05 10:12:48 +0330

    textfixed

--- src/components/slogan/index.tsx
+++ src/components/slogan/index.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Slogan() {
   return (
-    <div className='w-[90%] lg:w-[60%] text-center overflow-clip'>
-        <div className='mb-2'>مرجع آگهی ایران</div>
-        <div className='mb-2 text-xs lg:text-sm'>
+    <div className='w-[90%] lg:w-[70%] text-center overflow-clip'>
+        <div className=''>مرجع آگهی ایران</div>
+        <div className='mb-4 text-xs lg:text-sm'>
             مثل آب خوردن تو متاورس چیز میزاتو بفروش و واسه بیزینست تبلیغ کن
         </div>
-        <div className='text-xs lg:text-sm'>
+        <div className=''>
             ما از شما برای آگهی گذاشتن پول میگیریم؟
         </div>
         <div className='text-xs lg:text-sm mb-4'>
-            شما با گذاشتن آگهی هاتون به بزرگ شدن حرفه ما کمک میکنید. نه تنها پول نمیگیریم بلکه تلاش میکنیم شما رو تو این پیشرفت سهیم کنیم
+          هر آگهی که شما اینجا میزارید در واقع دارین به بزرگ شدن حرفه ی ما هم کمک میکنید پس ما نه تنها پولی ازتون نمیگیریم بلکه سعی میکنیم شما رو هم تو این پیشرفت سهیم کنیم
         </div>
         <div className='mb-2 text-xs'>BlackDoor پروژه ایرانی تجربه زندگی متاورسی</div>
         <div className='mb-2'></div>
