d4afdf50a8d0c231b4475512a707003838a46340
mohammad <M@>
2023-11-05 09:45:31 +0330

    fontsizemanuplate

--- src/components/slogan/index.tsx
+++ src/components/slogan/index.tsx
@@ -4,13 +4,13 @@ function Slogan() {
   return (
     <div className=' w-[60%] text-center overflow-clip'>
         <div className='mb-2'>مرجع آگهی ایران</div>
-        <div className='mb-2 text-sm'>
+        <div className='mb-2 text-xs lg:text-sm'>
             مثل آب خوردن تو متاورس چیز میزاتو بفروش و واسه بیزینست تبلیغ کن
         </div>
-        <div className='text-sm'>
+        <div className='text-xs lg:text-sm'>
             ما از شما برای آگهی گذاشتن پول میگیریم؟
         </div>
-        <div className='text-sm mb-4'>
+        <div className='text-xs lg:text-sm mb-4'>
             شما با گذاشتن آگهی هاتون به بزرگ شدن حرفه ما کمک میکنید. نه تنها پول نمیگیریم بلکه تلاش میکنیم شما رو تو این پیشرفت سهیم کنیم
         </div>
         <div className='mb-2 text-xs'>BlackDoor پروژه ایرانی تجربه زندگی متاورسی</div>
