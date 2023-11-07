6d33a0e94cdfbf395669abf57b439dfd7172dec2
mohammad <M@>
2023-11-08 00:16:30 +0330

    marginresize

--- src/components/citySearchBar/index.tsx
+++ src/components/citySearchBar/index.tsx
@@ -4,9 +4,9 @@ import searchIcon from '../../../public/icon/search.png'
 
 function CitySearchBar() {
   return (
-    <div className='w-[90%] md:w-[50%] h-[2rem] mt-6 pt-[2px] rounded-e-full flex justify-end pe-2 border-2 border-gray-300'>
-      <input placeholder='... شهرتو پیدا کن' className='me-4 md:me-8 w-full border-none outline-none ps-3 text-end text-sm pt-[2px] text-gray-400'/>
-      <div className='mt-[2px] opacity-50'>
+    <div className='w-[90%] md:w-[50%] h-[2rem] mt-4 rounded-e-full flex justify-end pe-2 border-2 border-gray-300'>
+      <input placeholder='... شهرتو پیدا کن' className='me-4 md:me-8 w-full border-none outline-none ps-3 text-end text-sm text-gray-400'/>
+      <div className='mt-[5px] opacity-50'>
         <Image src={searchIcon} alt='شهرتو پیدا کن' width={20} />
       </div>
     </div>
--- src/components/slogan/index.tsx
+++ src/components/slogan/index.tsx
@@ -4,16 +4,16 @@ function Slogan() {
   return (
     <div className='w-[90%] lg:w-[70%] text-center overflow-clip mt-8 text-gray-700'>
         <div className='font-bold'>مرجع آگهی ایران</div>
-        <div className='mb-4 text-xs lg:text-sm'>
+        <div className='mb-4 mt-1 text-xs lg:text-sm'>
             مثل آب خوردن تو متاورس چیز میزاتو بفروش و واسه بیزینست تبلیغ کن
         </div>
         <div className='font-bold'>
             ما از شما برای آگهی گذاشتن پول میگیریم؟
         </div>
-        <div className='text-xs lg:text-sm mb-4'>
+        <div className='text-xs mt-1 lg:text-sm'>
           هر آگهی که شما اینجا میزارید در واقع دارین به بزرگ شدن حرفه ی ما هم کمک میکنید پس ما نه تنها پولی ازتون نمیگیریم بلکه سعی میکنیم شما رو هم تو این پیشرفت سهیم کنیم
         </div>
-        <div className='mb-2 text-xs'>BlackDoor پروژه ایرانی تجربه زندگی متاورسی</div>
+        <div className='mt-6 text-xs'>BlackDoor پروژه ایرانی تجربه زندگی متاورسی</div>
     </div>
   )
 }
