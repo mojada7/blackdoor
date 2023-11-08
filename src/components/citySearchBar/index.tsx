9a6e43084dbabf6a8a637f24cc1a39bd99f3f920
mohammad <M@>
2023-11-08 22:06:05 +0330

    prvntdeftry

--- src/components/citySearchBar/index.tsx
+++ src/components/citySearchBar/index.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 import searchIcon from '../../../public/icon/search.png'
 
 function CitySearchBar() {
+  const prvntDef = (e : any)=> {
+    e.preventDefault()
+  }
   return (
     <div className='w-[90%] md:w-[50%] h-[2rem] mt-4 rounded-e-full flex justify-end pe-2 border-2 border-gray-300'>
-      <input placeholder='... شهرتو پیدا کن' className='me-4 md:me-8 w-full border-none outline-none ps-3 text-end text-sm text-gray-400 '/>
+      <input onFocus={(e)=>{prvntDef(e)}} placeholder='... شهرتو پیدا کن' className='me-4 md:me-8 w-full border-none outline-none ps-3 text-end text-sm text-gray-400 '/>
       <div className='mt-[5px] opacity-50'>
         <Image src={searchIcon} alt='شهرتو پیدا کن' width={20} />
       </div>
